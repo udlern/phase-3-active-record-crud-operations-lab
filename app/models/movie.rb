f38f2fd448d44a3a76f1b1fd2e14769a92b1e488
Noa Udler <udlernoa4@gmail.com>
2021-12-21 12:02:42 -0800

    commit tree 17e5532c17029cdbbae254f2200195523dcdb2f4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,35 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title(title)
+        Movie.find_or_create_by(title: title)
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        self.last
+    end
+    def self.movie_count
+        self.count
+    end
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+    def self.find_movie_with_attributes(title)
+        self.find_by(title)
+    end
+def self.find_movies_after_2002
+    self.where("release_date > ?", 2002)
+end
+def update_with_attributes(title)
+    self.update(title)
+end
+def self.update_all_titles(title)
+    self.update_all(title: title)
+end
+def self.delete_by_id(id)
+    self.destroy(id)
+end
+def self.delete_all_movies
+    self.destroy_all
+end
 end
